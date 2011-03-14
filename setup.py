fc7ac8d40559187855b5bcf802dd0325f9a6217c
andrew <sleepdev@gmail.com>
2011-03-13 21:53:06 -0700

    Trying to work out deployment of static files.

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from distutils.core import setup
 setup(
     name="APA division 44 website - quick deploy",
     packages=['app'],
+    package_data={  
+        'app': ['static/*','views/*.html','views/member/*.html'],
+    },
     scripts=['deploy'],
     license="PSF",
 )
